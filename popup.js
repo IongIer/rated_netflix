3822e50f622baecce1f1f33217264c19833fe1c5
oni <mosionroatarevine@gmail.com>
2022-07-09 11:14:01 +0300

    Add popup functionality

--- popup.js
+++ popup.js
@@ -1,5 +1,27 @@
-document.getElementById('update').addEventListener('click', function(){
-    let apikey = document.getElementById('apikey').value;
-    browser.storage.local.set({'apikey':apikey+'&t='});
-    document.getElementById('apikey').value ='';
+let key = browser.storage.local.get("apikey");
+key.then((result) => {
+  if ("apikey" in result) {
+    document.getElementById("indicator").style.color = "green";
+  } else {
+    document.getElementById("indicator").style.color = "red";
+  }
+});
+
+function storeKey() {
+  let apikey = document.getElementById("apikey").value;
+  if (apikey !== "") {
+    browser.storage.local.set({ apikey: apikey + "&t=" });
+    document.getElementById("apikey").value = "";
+    document.getElementById("indicator").style.color = "green";
+  }
+}
+
+document.getElementById("update").addEventListener("click", () => {
+  storeKey();
+});
+
+document.getElementById("apikey").addEventListener("keypress", (k) => {
+  if (k.key === "Enter") {
+    storeKey();
+  }
 });
